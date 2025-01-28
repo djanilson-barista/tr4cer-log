c6431925ae5581bedd5032582b9c88795ca393a0
djanilson alves <djanilson.alves@gmail.com>
2025-01-28 17:17:42 -0300

    feat: 🎸 add search, ui, clear logs

--- background.js
+++ background.js
@@ -13,11 +13,13 @@
 
 // chrome.runtime.onMessage.addListener(messageListener);
 
-chrome.action.onClicked.addListener(() => {
+function openApp(tab) {
   chrome.windows.create({
     url: chrome.runtime.getURL("popup.html"), // O arquivo HTML do popup
     type: "popup",
     width: 400,
     height: 600,
   });
-});
+}
+
+chrome.action.onClicked.addListener(openApp);
--- popup.js
+++ popup.js
@@ -1,27 +1,68 @@
 document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
-  const container = document.getElementById("logs-container");
+  var mockData = [
+    [
+      ">> DEV Track: [login_click_email] ",
+      {
+        shopcode: "not-fond",
+        storeName: "",
+        storePlan: "not-fond",
+        storeMail: "",
+        owner: "not-fond",
+        origin_domain: "http://localhost:3000",
+        origin: "PC",
+        screen_size: "1829x1029",
+        eventName: "input_interaction",
+        arguments: ["enter_email", "login"],
+      },
+    ],
+    [
+      ">> LOG TRACK: [forgot_password]",
+      {
+        event: "genericEvent",
+        event_name: "forgot_password",
+      },
+    ],
+    [
+      ">> DEV Track: [login_click_forgot_password] ",
+      {
+        shopcode: "not-fond",
+        storeName: "",
+        storePlan: "not-fond",
+        storeMail: "",
+        owner: "not-fond",
+        origin_domain: "http://localhost:3000",
+        origin: "PC",
+        screen_size: "1829x1029",
+        eventName: "forgot_password",
+        arguments: [],
+      },
+    ],
+    [
+      ">> LOG TRACK: [input_interaction]",
+      {
+        event: "genericEvent",
+        event_name: "input_interaction",
+        event_parameter_1: "enter_email",
+        event_parameter_2: "recovery_password",
+      },
+    ],
+    [
+      ">> LOG TRACK: [input_interaction]",
+      {
+        event: "genericEvent",
+        event_name: "input_interaction",
+        event_parameter_1: "enter_email",
+        event_parameter_2: "recovery_password",
+      },
+    ],
+  ];
 
-  const msgsReceived = [];
-  const _filteredMsgs = []; // filtered messages
+  const _container = document.getElementById("logs-container");
+
+  let _searchDelay = null;
+  let _msgsReceived = [];
+  let _filteredMsgs = []; // filtered messages
   let _inSearch = false;
-
-  // Search logs in msgsReceived
-  // function searchLogs() {
-  //   _inSearch = true;
-  //   const searchInput = document.getElementById("search-input");
-  //   const searchValue = searchInput.value.toLowerCase();
-
-  //   msgsReceived.map((logData) => {
-  //     const logItem = document.querySelector(".log-item");
-
-  //     if (JSON.stringify(logData.details).toLowerCase().includes(searchValue)) {
-  //       logItem.style.display = "block";
-  //       _filteredMsgs.unshift(logData);
-  //     } else {
-  //       logItem.style.display = "none";
-  //     }
-  //   });
-  // }
 
   function toggleDetails(target) {
     const logHeader = target;
@@ -44,7 +85,21 @@ document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
   }
 
   function renderLogs() {
-    msgsReceived.map((message) => {
+    _container.innerHTML = "";
+    const list = _inSearch ? _filteredMsgs : _msgsReceived;
+
+    if (list.length === 0) {
+      _container.innerHTML = `
+      <p id="instructions">
+        Seu log irá aparecer aqui quando ele for interceptado.
+      </p>`;
+    }
+
+    document.querySelector("#logs-count").innerText = list.length
+      ? `(${list.length})`
+      : "";
+
+    list.map((message) => {
       const title = message[0];
       let details = JSON.stringify(message[1], null, 2);
 
@@ -58,28 +113,72 @@ document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
         </div>
       `;
 
-      container.insertAdjacentHTML("afterbegin", logHTML);
+      _container.insertAdjacentHTML("afterbegin", logHTML);
     });
   }
 
   function handleMessage(message) {
     console.log("[QA Logger] Mensagem recebida no popup:", message);
 
-    document.querySelector("#instructions").style.display = "none";
+    if (document.querySelector("#instructions")) {
+      document.querySelector("#instructions").style.display = "none";
+    }
 
     if (message.type === "QA_LOG") {
-      if (msgsReceived.length >= 100) {
-        msgsReceived.length = 0;
+      if (_msgsReceived.length >= 100) {
+        _msgsReceived.length = 0;
       }
 
       // add in beginning of the array
-      msgsReceived.unshift(message.payload);
+      _msgsReceived.unshift(message.payload);
       renderLogs();
     }
   }
 
-  // Escuta mensagens do background
-  chrome.runtime.onMessage.addListener(handleMessage);
+  function searchLogs(searchValue) {
+    _inSearch = true;
+    _msgsReceived.map((logData) => {
+      const details = logData[1];
+
+      if (JSON.stringify(details).toLowerCase().includes(searchValue)) {
+        _filteredMsgs.push(logData);
+      }
+
+      renderLogs();
+    });
+  }
+
+  function handleSearch({ target }) {
+    _searchDelay && clearTimeout(_searchDelay);
+
+    setTimeout(() => {
+      _inSearch = false;
+      _filteredMsgs = [];
+
+      if (target.id === "search-input") {
+        searchLogs(target.value);
+      }
+    }, 800);
+  }
+
+  // // debugger;
+  const debuggerMode = false;
+
+  if (!debuggerMode) {
+    // Escuta mensagens do background
+    chrome.runtime.onMessage.addListener(handleMessage);
+  } else {
+    _msgsReceived = mockData;
+    renderLogs();
+  }
 
   document.addEventListener("click", handleLogHeaderClick);
+  document.addEventListener("click", ({ target }) => {
+    if (target.id === "clear-logs") {
+      _msgsReceived.length = 0;
+      renderLogs();
+    }
+  });
+
+  document.addEventListener("keyup", handleSearch);
 });
