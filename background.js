36a451514507b2cdcc5e67c36dd5fa32e07532c5
djanilson alves <djanilson.alves@gmail.com>
2025-01-29 14:10:26 -0300

    refactor: 💡 collapse dos log-itens

--- background.js
+++ background.js
@@ -1,18 +1,4 @@
-// // background.js
-
-// function messageListener(message, sender, sendResponse) {
-//   console.log("[QA Logger] Mensagem recebida no background:", message);
-
-//   if (message.type === "QA_LOG_BG") {
-//     // Envia a mensagem para o popup
-//     console.log("[QA Logger] Log recebido:", message.payload);
-
-//     chrome.runtime.sendMessage({ type: "QA_LOG", payload: message.payload });
-//   }
-// }
-
-// chrome.runtime.onMessage.addListener(messageListener);
-
+// background.js
 function openApp(tab) {
   chrome.windows.create({
     url: chrome.runtime.getURL("popup.html"), // O arquivo HTML do popup
--- popup.js
+++ popup.js
@@ -1,72 +1,12 @@
 document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
-  var mockData = [
-    [
-      ">> DEV Track: [login_click_email] ",
-      {
-        shopcode: "not-fond",
-        storeName: "",
-        storePlan: "not-fond",
-        storeMail: "",
-        owner: "not-fond",
-        origin_domain: "http://localhost:3000",
-        origin: "PC",
-        screen_size: "1829x1029",
-        eventName: "input_interaction",
-        arguments: ["enter_email", "login"],
-      },
-    ],
-    [
-      ">> LOG TRACK: [forgot_password]",
-      {
-        event: "genericEvent",
-        event_name: "forgot_password",
-      },
-    ],
-    [
-      ">> DEV Track: [login_click_forgot_password] ",
-      {
-        shopcode: "not-fond",
-        storeName: "",
-        storePlan: "not-fond",
-        storeMail: "",
-        owner: "not-fond",
-        origin_domain: "http://localhost:3000",
-        origin: "PC",
-        screen_size: "1829x1029",
-        eventName: "forgot_password",
-        arguments: [],
-      },
-    ],
-    [
-      ">> LOG TRACK: [input_interaction]",
-      {
-        event: "genericEvent",
-        event_name: "input_interaction",
-        event_parameter_1: "enter_email",
-        event_parameter_2: "recovery_password",
-      },
-    ],
-    [
-      ">> LOG TRACK: [input_interaction]",
-      {
-        event: "genericEvent",
-        event_name: "input_interaction",
-        event_parameter_1: "enter_email",
-        event_parameter_2: "recovery_password",
-      },
-    ],
-  ];
-
   const _container = document.getElementById("logs-container");
-
   let _searchDelay = null;
   let _msgsReceived = [];
   let _filteredMsgs = []; // filtered messages
   let _inSearch = false;
 
   function toggleDetails(target) {
-    const logHeader = target;
-    const logDetails = logHeader.nextElementSibling;
+    const logDetails = target;
     if (
       logDetails.style.display === "none" ||
       logDetails.style.display === ""
@@ -79,8 +19,13 @@ document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
 
   function handleLogHeaderClick(event) {
     // identifica se o item clicado é um log header
-    if (event.target.className === "log-header") {
-      toggleDetails(event.target);
+    if (event.target.className.includes("toggle-act")) {
+      // suba até o log-item
+      const logItem = event.target
+        .closest(".log-item")
+        .querySelector("#logDetails");
+
+      toggleDetails(logItem);
     }
   }
 
@@ -99,15 +44,16 @@ document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
       ? `(${list.length})`
       : "";
 
-    list.map((message) => {
+    list.forEach((message, idx) => {
+      const num = idx + 1 > 9 ? idx + 1 : `0${idx + 1}`;
       const title = message[0];
       let details = JSON.stringify(message[1], null, 2);
 
       // Cria um novo item de log
       const logHTML = `
         <div class="log-item">
-          <div class="log-header">
-            ${title}
+          <div class="log-header toggle-act">
+            <span class="toggle-act">${title}</span> <small>#${num}</small> 
           </div>
           <pre class="log-details" id="logDetails">${details}</pre>
         </div>
@@ -130,17 +76,21 @@ document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
       }
 
       // add in beginning of the array
-      _msgsReceived.unshift(message.payload);
+      _msgsReceived.push(message.payload);
       renderLogs();
     }
   }
 
   function searchLogs(searchValue) {
     _inSearch = true;
-    _msgsReceived.map((logData) => {
-      const details = logData[1];
+    _msgsReceived.forEach((logData) => {
+      const details = logData;
 
-      if (JSON.stringify(details).toLowerCase().includes(searchValue)) {
+      if (
+        JSON.stringify(details)
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      ) {
         _filteredMsgs.push(logData);
       }
 
@@ -161,15 +111,41 @@ document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
     }, 800);
   }
 
-  // // debugger;
-  const debuggerMode = false;
+  function runDebugger() {
+    _msgsReceived = [
+      [
+        ">> LOG TRACK: [forgot_password]",
+        {
+          event: "genericEvent",
+          event_name: "forgot_password",
+        },
+      ],
+      [
+        ">> DEV Track: [login_click_forgot_password] ",
+        {
+          shopcode: "not-fond",
+          storeName: "",
+          storePlan: "not-fond",
+          storeMail: "",
+          owner: "not-fond",
+          origin_domain: "http://localhost:3000",
+          origin: "PC",
+          screen_size: "1829x1029",
+          eventName: "forgot_password",
+          arguments: [],
+        },
+      ],
+    ];
+    renderLogs();
+  }
 
-  if (!debuggerMode) {
+  const _debuggerMode = false;
+
+  if (_debuggerMode) {
+    runDebugger();
+  } else {
     // Escuta mensagens do background
     chrome.runtime.onMessage.addListener(handleMessage);
-  } else {
-    _msgsReceived = mockData;
-    renderLogs();
   }
 
   document.addEventListener("click", handleLogHeaderClick);
