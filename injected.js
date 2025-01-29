74f9adb1d948c6e684a7385731a51401229f7735
djanilson alves <djanilson.alves@gmail.com>
2025-01-29 16:57:51 -0300

    fix: 🐛 (injected) corrige o log padrao q estava indo como array

--- injected.js
+++ injected.js
@@ -1,18 +1,16 @@
 (function () {
   // Salva o console.log original
-  const log = console.log.bind(console);
+  const log = console.log;
 
   // Adiciona um listener para enviar logs ao background
   console.log = (...args) => {
-    log(args);
-
     if (!(args[0].includes(">> LOG") || args[0].includes(">> DEV Track"))) {
+      log(...args);
+
       return;
     }
 
     // Envia o log ao background
     window.postMessage({ type: "QA_LOG", payload: args }, "*");
-    // Chama o console.log original
-    log.apply(console, args);
   };
 })();
